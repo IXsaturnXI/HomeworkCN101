8af313f8b081b5bd61a2cfce8d65f0fa645e8e3b
IXsaturnXI <bdkp123@gmail.com>
2025-10-24 12:56:03 +0700

    Add Hi Gen

--- Calculator1.py
+++ Calculator1.py
@@ -1,3 +1,5 @@
 A = float(input("A digit value is "))
 B = float(input("B digit value is "))
 print("A+B=",A+B,"\n""A-B=",A-B,"\n""A*B=",A*B,"\n""A/B=",A/B,"\n""A//B=",A//B,"\n""A%B=",A%B,"\n""A**B=",A**B,"\n")
+
+print("Hi Gen")
